63d516c639673997773ff3012307cddf12993f0d
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-23 23:54:28 +0100

    aggiunto h1

---
+++ myscript.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue (
+    {
+        el: '#h1',
+        data: {
+            message: 'Vue prova',
+        }
+    }
+);
